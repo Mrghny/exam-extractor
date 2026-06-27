7ba865d920cfbb3361a4b352fd21b9d3fbbf4b2f
Mrghny <marwanmoatassem@proton.me>
2026-06-27 19:43:27 +0300

    Logging errors to a log file & Enabling manual fixing of file names if data is not found. Counting number of errors.

--- main.py
+++ main.py
@@ -6,36 +6,85 @@ import re
 paper_names = ["Pure Mathematics P1", "Pure Mathematics P2", "Pure Mathematics P3", "Pure Mathematics P4"]
 shortened_paper_names = ["pm1", "pm2", "pm3", "pm4"]
 short_paper_names = ["p1","p2","p3","p4"]
+
 paper_codes = ["WMA11", "WMA12", "WMA13", "WMA14"]
 
 session_names = ["january", "may"]
 short_session_names = ["jan", "may"]
+
+file_name_pattern = rf"^\d{{4}}_({'|'.join(session_names)})_({'|'.join(shortened_paper_names)})$"
+
+
 qp_pdfs_path = "./exams"
 ms_pdfs_path = "./mark_schemes"
-# get_qp_info(paper_names, shortened_paper_names)
-# get_ms_info(paper_names, shortened_paper_names)
+
+log_file = "errors.txt"
+
+err_amount = 0
+no_of_pdfs = max(len(os.listdir(qp_pdfs_path)), len(os.listdir(ms_pdfs_path)))
 
 
 # Create needed folders and files
-def folder_setup(): 
+def folder_setup():
+    global err_amount
+
     os.makedirs("output", exist_ok=True)
+    
+    # Generate error folders
+    os.makedirs("errors/qp/duplicates", exist_ok=True)
+    os.makedirs("errors/qp/data_not_found", exist_ok=True)
+    os.makedirs("errors/qp/corrosponding", exist_ok=True)
+
+    os.makedirs("errors/ms/duplicates", exist_ok=True)
+    os.makedirs("errors/ms/data_not_found", exist_ok=True)
+    os.makedirs("errors/ms/corrosponding", exist_ok=True)
+
+    os.makedirs("errors/question_amount/", exist_ok=True)
+
     files = []
+    ms_files = os.listdir(ms_pdfs_path)
+    qp_files = os.listdir(qp_pdfs_path)
+    
+    # Check if a qp file has a corrosponding ms file and vice-versa
+    for p in qp_files:
+        if p not in ms_files:
+            os.rename(f"{qp_pdfs_path}/{p}",f"./errors/qp/corrosponding/{p}")
+            err_amount += 1
+            with open(log_file, "a") as f:
+                f.write(f"Corrosponding File Not Found: {qp_pdfs_path}/{p} --> errors/qp/corrosponding/{p}\n")
+
+    for p in ms_files:
+        if p not in qp_files:
+            os.rename(f"{ms_pdfs_path}/{p}",f"./errors/ms/corrosponding/{p}")
+            err_amount += 1
+            with open(log_file, "a") as f:
+                f.write(f"Corrosponding File Not Found: {ms_pdfs_path}/{p} --> errors/ms/corrosponding/{p}\n")
+
+
+    # Create the necessary folders for files that passed
     for p in os.listdir(qp_pdfs_path):
         file_name = os.path.splitext(p)[0]
         os.makedirs(f"./output/{file_name}/qp_screenshots", exist_ok=True)
         os.makedirs(f"./output/{file_name}/ms_screenshots", exist_ok=True)
 
-
-
 # Find year, session, paper name. Then change file name
 def check_qp(): 
+    global err_amount
     cnt = 0
+    dup = 0
     for path in os.listdir(qp_pdfs_path):
-        original_path = path
+        file_name = os.path.splitext(path)[0]
+
+        # If the names are already done skip
+        if re.match(file_name_pattern, file_name):
+            continue
+
+        original_path = f"{qp_pdfs_path}/{path}"
         path = path.lower()
         session = None
         paper_name = None
         year = None
+
         # Finding data through the file name
         for s in session_names:
             s = s.lower()
@@ -64,9 +113,9 @@ def check_qp():
                 if path.find(p) >= 0:
                     paper_name = shortened_paper_names[idx]
                     break
-
+        
         # Finding data using the PDF
-        doc = pymupdf.open(f"{qp_pdfs_path}/{original_path}")
+        doc = pymupdf.open(original_path)
         page = doc[0]
         text = page.get_text().lower()
         
@@ -95,20 +144,32 @@ def check_qp():
         if not year:
             year = f"na"
 
-
+        
 
         exam = f"{year}_{session}_{paper_name}"
-        folders = os.listdir(f"{qp_pdfs_path}")
-        file = f"{exam}.pdf"
-        if file in folders and not file.find("na"):
-            exam = f"{exam}_{cnt}"
-            cnt+=1
         
-        os.rename(f"{qp_pdfs_path}/{original_path}", f"{qp_pdfs_path}/{exam}.pdf")
-    
+
+        try:
+            if exam.find("na") != -1:
+                cnt += 1 
+                os.rename(original_path, f"errors/qp/data_not_found/{exam}_{cnt}.pdf")
+                with open(log_file, "a") as f:
+                    err_amount += 1
+                    f.write(f"DataNotFound: {original_path} --> errors/qp/data_not_found/{exam}_{cnt}.pdf\n")
+                
+            else:
+                os.rename(original_path, f"{qp_pdfs_path}/{exam}.pdf")
+        
+        except FileExistsError:
+            os.rename(original_path, f"errors/qp/duplicates/{exam}_{dup}.pdf")
+            with open(log_file, "a") as f:
+                err_amount += 1
+                f.write(f"FileExistsError: {original_path} --> errors/qp/duplicates/{exam}_{dup}.pdf\n")
+            dup += 1
 
 # Screenshot each question in each qp and then setup json file to link screenshots with known data (question number, amount of marks in the question)
 def get_qp_info(): 
+    global err_amount
 
     for path in os.listdir(qp_pdfs_path):
         file_name = os.path.splitext(path)[0]
@@ -119,12 +180,11 @@ def get_qp_info():
                 data[2] = paper_names[idx]
 
 
-
         output = {
             "exam":{
                 "year": int(data[0]),
-            "session": data[1],
-            "paper": data[2]
+                "session": data[1],
+                "paper": data[2]
             },
             "questions": []
         }
@@ -152,8 +212,6 @@ def get_qp_info():
             q.append(no_screenshots) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         
         
-        # Later: make it so if a paper uses "QNumber:" instead of "QNumber." it still detects it
-
         check = False 
         p_num = 0
         no_screenshots = 0
@@ -204,9 +262,17 @@ def get_qp_info():
 
 # Find year, session, paper name from ms pdf. Then change file name
 def check_ms():
+    global err_amount
     cnt = 0
+    dup = 0
     for path in os.listdir(ms_pdfs_path):
-        original_path = path
+
+        file_name = os.path.splitext(path)[0]
+        # If the names are already done skip
+        if re.match(file_name_pattern, file_name):
+            continue
+
+        original_path = f"{ms_pdfs_path}/{path}"
         path = path.lower()
         session = None
         paper_name = None
@@ -228,10 +294,10 @@ def check_ms():
         
 
         if not session:
-            for s in short_session_names:
+            for idx,s in enumerate(short_session_names):
                 s = s.lower()
                 if path.find(s) >= 0:
-                    session = s
+                    session = session_names[idx]
                     break
 
         if not paper_name:
@@ -242,7 +308,7 @@ def check_ms():
                     break
         
         # Finding data using the PDF
-        doc = pymupdf.open(f"{ms_pdfs_path}/{original_path}")
+        doc = pymupdf.open(original_path)
         page = doc[0]
         text = page.get_text().lower()
         
@@ -287,16 +353,27 @@ def check_ms():
             year = f"na"
 
         exam = f"{year}_{session}_{paper_name}"
-        file = f"{exam}.pdf"
-
-        if file in os.listdir("./mark_schemes") and not file.find("na"):
-            exam = f"{exam}_{cnt}"
-            cnt+=1
-
-        os.rename(f"{ms_pdfs_path}/{original_path}", f"{ms_pdfs_path}/{exam}.pdf")
-
+        try:
+            if exam.find("na") != -1:
+                cnt += 1 
+                os.rename(original_path, f"errors/ms/data_not_found/{exam}_{cnt}.pdf")
+                with open(log_file, "a") as f:
+                    err_amount += 1
+                    f.write(f"DataNotFound: {original_path} --> errors/ms/data_not_found/{exam}_{cnt}.pdf\n")
+                
+            else:
+                os.rename(original_path, f"{ms_pdfs_path}/{exam}.pdf")
+        
+        except FileExistsError:
+            os.rename(original_path, f"errors/ms/duplicates/{exam}_{dup}.pdf")
+            with open(log_file, "a") as f:
+                err_amount += 1
+                f.write(f"FileExistsError: {original_path} --> errors/ms/duplicates/{exam}_{dup}.pdf\n")
+            dup += 1
+    
 
 def get_ms_info():
+    global err_amount
 
     for path in os.listdir(ms_pdfs_path):
         doc = pymupdf.open(f"{ms_pdfs_path}/{path}")
@@ -356,7 +433,7 @@ def get_ms_info():
             pages_per_q.append(page_number)
 
             question_number += 1
-            table_number = 1
+            table_number = 0
             for j in range(question_page, end_page):
                 page = doc[j]
                 tables = doc[j].find_tables()
@@ -369,13 +446,11 @@ def get_ms_info():
                     pix = page.get_pixmap(clip=bbox)
                     
                     # Save as PNG
-                    pix.save(f"{q_folder_path}/table/{table_number}.png")
+                    pix.save(f"{q_folder_path}/table/table{table_number}.png")
                     pix = None
                     table_number += 1
         
             tables_per_q.append(table_number)
-
-       
 
             
         # Editing JSON to link to the page images directly
@@ -384,10 +459,19 @@ def get_ms_info():
 
         # If the number of questions found in ms not equal the number of questions we got from qp
         if(len(pages_per_q) != len(py_dict["questions"])):
-            print(exam_folder_path)
+            os.makedirs(f"./errors/question_amount/{exam_folder_path}", exist_ok=True)
+            os.rename(f"{ms_pdfs_path}/{exam_folder_path}.pdf", f"./errors/question_amount/{exam_folder_path}/{exam_folder_path}_ms.pdf")
+            os.rename(f"{qp_pdfs_path}/{exam_folder_path}.pdf", f"./errors/question_amount/{exam_folder_path}/{exam_folder_path}_qp.pdf")
+            os.rename(f"./output/{exam_folder_path}", f"./errors/question_amount/{exam_folder_path}/{exam_folder_path}")
+            
+            with open(log_file, "a") as f:
+                err_amount += 1
+                f.write(f"Question Amount Difference: ms: {len(pages_per_q)}, qp: {len(py_dict["questions"])} --> moved {exam_folder_path}\n")
             continue
         else:
             for idx, question in enumerate(py_dict["questions"]):
+                question["mark_scheme_path"].append(f"{folder_to_save_in}/question{idx+1}/table/table0")
+
                 for i in range(1, pages_per_q[idx]):
                     question["mark_scheme_path"].append(f"{folder_to_save_in}/question{idx+1}/page{i}")
 
@@ -400,10 +484,18 @@ def get_ms_info():
         f.close()
 
 
-# check_qp()
-# folder_setup()    
 
 
-# get_qp_info()
-# check_ms()
-# get_ms_info()
+
+
+check_qp() 
+check_ms() 
+folder_setup() 
+
+
+
+
+get_qp_info()
+get_ms_info()
+
+print(f"Error Percentage = {(err_amount/no_of_pdfs)*100}%")
